a56b6d9b686ddffad0a1a6aa3b4ab9629f19a8ce
Travis CI User <travis@example.org>
2023-08-15 09:57:36 +0000

    Merge pull request #34 from Hyperkid123/sass-prefix
    Add missing sass prefix.

--- js/exposed-./RootApp.768e99804ad8d23e26f3.js
+++ js/exposed-./RootApp.768e99804ad8d23e26f3.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklearningResources=self.webpackChunklearningResources||[]).push([[178],{5730:(e,n,r)=>{r.r(n),r.d(n,{default:()=>u});var t=r(3264),s=r.n(t),a=r(7190),c=function(){return c=Object.assign||function(e){for(var n,r=1,t=arguments.length;r<t;r++)for(var s in n=arguments[r])Object.prototype.hasOwnProperty.call(n,s)&&(e[s]=n[s]);return e},c.apply(this,arguments)};const u=function(e){return s().createElement(a.g,c({},e))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.06393cf05f070d27c70d77bd9ebb9a50.js.map
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.407d811ac170d8551839fd9fb6577823.js.map
